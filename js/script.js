450b65efe83e41bfbe78deedeed64a3d50c15d90
Hazem Hassan Mahmoud <hazemhassan0321@gmail.com>
2024-08-19 19:42:55 +0300

    Image Gallery App

---
+++ js/script.js
@@ -0,0 +1,66 @@
+let images = Array.from(document.querySelectorAll(".slides img")),
+  imagesNumber = images.length,
+  currentSlide = 1,
+  currentIndex = document.getElementById("index"),
+  prev = document.getElementById("prev"),
+  next = document.getElementById("next");
+
+function prevSlide() {
+  if (currentSlide > 1) {
+    --currentSlide;
+    control();
+  } else if (currentSlide === 1) {
+    currentSlide = imagesNumber;
+    control();
+  }
+}
+function nextSlide() {
+  if (currentSlide < imagesNumber) {
+    ++currentSlide;
+    control();
+  } else if (currentSlide === imagesNumber) {
+    currentSlide = 1;
+    control();
+  }
+}
+prev.addEventListener("click", prevSlide);
+next.addEventListener("click", nextSlide);
+
+let nav = document.getElementById("indicators");
+for (let i = 1; i <= imagesNumber; i++) {
+  let navItem = document.createElement("img");
+  navItem.setAttribute("data-index", i);
+  navItem.setAttribute("src", `images/${i}.jpg`);
+  nav.append(navItem);
+}
+
+let navItems = document.querySelectorAll("#indicators img");
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    currentSlide = parseInt(item.getAttribute("data-index"));
+    control();
+  });
+});
+
+function removeActives() {
+  images.forEach((img) => {
+    img.classList.remove("active");
+  });
+  navItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+}
+
+function control() {
+  currentIndex.textContent = `Slide #${currentSlide} of ${imagesNumber}`;
+  removeActives();
+  images[currentSlide - 1].classList.add("active");
+  nav.children[currentSlide - 1].classList.add("active");
+  document.body.style.cssText += `
+  background-image: url(images/${currentSlide}.jpg);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  `;
+}
+control();
